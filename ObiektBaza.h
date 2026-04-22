d6cc96fbd0077d332c17cfee14b02731efd4dfce
MaiIneMie <maia.miernicka.stud@pw.edu.pl>
2026-04-22 14:59:48 +0200

    Dodano metodę generuj_s w ObiektBaza dla uproszczenia implementacji w obiektach.

--- ObiektBaza.h
+++ ObiektBaza.h
@@ -1,11 +1,26 @@
 #ifndef OBIEKTBAZA_H
 #define OBIEKTBAZA_H
 
+#include <Eigen/Dense>
+
 class ObiektBaza {
 public:
     virtual double krok_online(double u_k) = 0; // Deklaracja
     virtual void reset() = 0;
     virtual double get_stan() const = 0;
+
+    // Metoda dla generacji odpowiedzi skokowej
+    Eigen::VectorXd generuj_s(int D)
+    {   
+        reset(); // Wyczyszczenie pamięci dla obiektu dla bezpieczeństwa
+        Eigen::VectorXd s = Eigen::VectorXd::Zero(D); // Utworzenie wektora s i wypełnienie zerami
+        for (int i = 0; i < D; i++)
+        {
+            s(i) = krok_online(1.0); // Generacja odpowiedzi dla skoku jednostkowego
+        }
+        reset(); // Wyczyszczenie pamięci dla obiektu dla czystego startu regulacji
+        return s;
+    }
     virtual ~ObiektBaza() {}  // Destruktor
 };
 
